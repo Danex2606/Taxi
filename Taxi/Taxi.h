682744514a13c2665f8c71a0f5fd91455fac4ced
Danex2606 <don.alekseev26@yandex.ru>
2020-04-17 19:46:26 +0300

    Создание проекта

---
+++ Taxi/Taxi.h
@@ -0,0 +1,32 @@
+﻿
+// Taxi.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CTaxiApp:
+// Сведения о реализации этого класса: Taxi.cpp
+//
+
+class CTaxiApp : public CWinApp
+{
+public:
+	CTaxiApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTaxiApp theApp;
